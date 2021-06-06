b5b390b88908eff8e60128533105b73915b81dd8
andi-faizal94 <andifaizal647@gmail.com>
2021-06-06 18:52:39 +0700

    menambahkan cara kedua membuat todo

--- main.js
+++ main.js
@@ -2,14 +2,39 @@ const output = document.getElementById('output');
 
 function add() {
 
-    // mengamnbil value dari input
-    const input = document.getElementById('input-value');
-    console.log(input.value)
+    // cara pertama:
 
-    // menambahkan list baru kedalam ul 
-    const li = document.createElement('li');
-    const hasil = document.createTextNode(input.value);
+    // 1.mengamnbil value dari input
+    // const input = document.getElementById('input-value');
+    // console.log(input.value)
 
-    li.appendChild(hasil);
-    output.appendChild(li);
+    // 2.menambahkan list baru kedalam ul 
+    // const li = document.createElement('li');
+    // const hasil = document.createTextNode(input.value);
+    // 3.menggabungkan text kedalam li
+    // li.appendChild(hasil);
+    // 4.menggabungkan li kedalam output secara dinamis
+    // output.appendChild(li);
+
+    // cara yang kedua :
+
+    let input = document.getElementById('input-value');
+
+    let baru = "<li> <span onclick='toggle(this)'>" + input.value + "</span>" + "<span onclick='removeItems(this)'> [X] </span>" + "</li>";
+
+    output.insertAdjacentHTML('afterbegin', baru);
+
+    input.value = "";
+
+
+};
+
+function toggle(el) {
+    el.classList.toggle('done');
+
+};
+
+function removeItems(el) {
+    el.parentElement.remove();
+
 }
